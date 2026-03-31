6ff6e4dea1bf2ff45a8d84fda9afb1267f7fcaa0
lauramirilli <lcardinmirilli@gmail.com>
2026-03-31 11:17:59 -0300

    feat: solicitando informações ao usuário usando prompt()

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+let nome_vinho = prompt("Informe o nome do vinho: ")
+let tipo_vinho = prompt("Informe o tipo do vinho (Tinto, Branco, Rosé): ")
+let safra_vinho = prompt("Informe a safra do vinho: ")
+let qnt_vinho = prompt("Informe a quantidade do vinho em estoque: ")
