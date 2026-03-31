e67aee3da736f6877628529ce1be44512e85d8fe
Bruce Li <bruceetng@gmail.com>
2026-03-31 11:40:24 -0300

    feat: Adicionando alert()

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,5 @@ let vinho = prompt("Informe o nome do vinho: ")
 let tipo = prompt("Informe o tipo do vinho (Tinto, Branco, Rosé): ")
 let safra = prompt("Informe a safra do vinho: ")
 let qnt = prompt("Informe a quantidade do vinho em estoque: ")
+
+alert("Cadastro Realizado! Veja os detalhes no console.")
